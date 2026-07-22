4aeded60b75e450f026e5827da623806ed12827d
briensbe <briensbe@yahoo.com>
2026-07-22 21:07:48 +0200

    clé de localStorage pour les releases_notes spécifique

--- src/app/shared/release-notes/release-notes.component.ts
+++ src/app/shared/release-notes/release-notes.component.ts
@@ -49,7 +49,7 @@ export class ReleaseNotesComponent implements OnInit, OnDestroy {
 
   showHistory = false;
   activeImageUrl: string | null = null;
-  private readonly STORAGE_KEY = 'last_seen_release_version';
+  private readonly STORAGE_KEY = 'crewdayz_last_seen_release_version';
   private subscription: Subscription | null = null;
   private hasBeenClosedInSession = false;
 
